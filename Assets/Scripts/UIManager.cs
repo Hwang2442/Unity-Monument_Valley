8bf8ca335de0c1d4181054cceb9f8d152d1eb0d3
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2020-09-24 20:51:41 +0900

    UI 추가
    버튼 추가 및 클릭 이벤트 추가중

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Button stage1;
+    public Button stage2;
+
+    void Update()
+    {
+        
+    }
+}
