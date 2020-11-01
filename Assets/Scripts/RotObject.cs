24371d0887010bdf4300331902765906db17ffa8
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2020-11-01 19:40:24 +0900

    스테이지 2 회전 오류 수정

--- Assets/Scripts/RotObject.cs
+++ Assets/Scripts/RotObject.cs
@@ -43,17 +43,28 @@ public class RotObject : MonoBehaviour
             if(Physics.Raycast(mouseRay, out mouseHit))
             {
                 // 충돌한 오브젝트가 레버인지 검사
-                for (int i = 0; i < transform.childCount; i++)
+                if (mouseHit.transform == transform)
                 {
-                    if (mouseHit.transform == transform.GetChild(i))
+                    isRotate = true;
+
+                    pastAngle = getAngle();
+
+                    soundNum = 0;
+                }
+                else
+                {
+                    for (int i = 0; i < transform.childCount; i++)
                     {
-                        isRotate = true;
+                        if (mouseHit.transform == transform.GetChild(i))
+                        {
+                            isRotate = true;
 
-                        pastAngle = getAngle();
+                            pastAngle = getAngle();
 
-                        soundNum = 0;
+                            soundNum = 0;
 
-                        break;
+                            break;
+                        }
                     }
                 }
             }
